31708f249501b5c5ec4a8d58ae12230ff04ba7f4
Mamdouh Basha <m.basha@sap.com>
2020-05-14 14:02:03 -0400

    test a small change in consul chart

--- .github/actions/helm-lint/run.sh
+++ .github/actions/helm-lint/run.sh
@@ -2,7 +2,6 @@
 helm1=$(helm lint bitnami/apache | tail -1)
 
 if [ -z "$helm1" ]; then
-    echo "::set-output name=output1::failed"
     exit 1
 else
     echo "::set-output name=output1::$helm1"
@@ -11,7 +10,6 @@ fi
 helm2=$(helm lint bitnami/consul | tail -1)
 
 if [ -z "$helm2" ]; then
-    echo "::set-output name=output2::failed"
     exit 1
 else
     echo "::set-output name=output2::$helm2"
